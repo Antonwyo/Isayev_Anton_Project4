214870ec5da282a91866007b58d7d7dab57af04d
Anton Isayev <Antonwyo@fullsail.edu>
2012-09-20 10:36:12 -0600

    Added URL function

--- Isayev_Anton_Project4.js
+++ Isayev_Anton_Project4.js
@@ -16,21 +16,28 @@ var myLibrary = function(){
 		
 	};
 	
-		return { 
-			"checkNumber":checkNumber
-		
+	// check URL
+	var checkURL = function(val){
+		var first = val.substring(0,val.indexOf(":"));
+		if((first === "http") || (first === "https")){
+			return true;
+		} else {
+			return false;
 		};
-	
 	};
+
 	
 	
 	
 	
-	
-	
-	
+	return { 
+			"checkNumber":checkNumber,
+			"checkURL": checkURL
+		};
+	};
 	
 	var nL  = new myLibrary();
 	console.log(nL.checkNumber(5));
+	console.log(nL.checkURL("yahoo.com"));
 	
 	
